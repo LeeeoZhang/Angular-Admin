c56b12dd14accafa3b13f6e7bb2ba936a4c7a6a7
Leo Cheung <leozhang621@gmail.com>
2018-10-24 16:53:06 +0800

    add crisis-center

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,25 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
-import {CrisisListComponent} from './crisis-list/crisis-list.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 import {AppRoutingModule} from './app-routing.module';
 import {HeroesModule} from './heroes/heroes.module';
+import {CrisisCenterModule} from './crisis-center/crisis-center.module';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        CrisisListComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
+        ComposeMessageComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
         HeroesModule,
+        CrisisCenterModule,
         AppRoutingModule,
     ],
     providers: [
